09873c5dc8f6e9a3d8b282c9702ef5ec2b07d573
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-08-31 16:07:24 -0300

    criacao da entidade productModel

--- src/main/java/com/crud_produtos/api/model/ProductModel.java
+++ src/main/java/com/crud_produtos/api/model/ProductModel.java
@@ -1,8 +1,13 @@
 package com.crud_produtos.api.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -10,5 +15,41 @@ import jakarta.persistence.Table;
 public class ProductModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+
+    private String name;
+
+    private BigDecimal value;
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 
 }
